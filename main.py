1e28428af255df3b75cbc3b9b288c47834dde2cc
ArelaVka <arelav90@mail.ru>
2019-08-29 11:29:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import os
 def get_new_comics():
     url = 'https://xkcd.com/info.0.json'
     response_json = requests.get(url).json()
-    img_link = response_json['img']
+    img_link = response_json["img"]
     filename = img_link.split('/')[-1]
+    response = requests.get(img_link)
+    response.raise_for_status()
     with open(filename, 'wb') as file:
-            file.write(requests.get(img_link).content)
+        file.write(response.content)
 
 if __name__ == '__main__':
     get_new_comics()
     # print('Download from {}'.format(get_new_comics()))
+
